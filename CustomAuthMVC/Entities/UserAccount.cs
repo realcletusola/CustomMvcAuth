c6addf3aa162be5c170f7297487c42630066627e
cletus <cletushunsu@gmail.com>
2024-08-27 08:12:13 -0700

    created UserAccount entity

---
+++ CustomAuthMVC/Entities/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomAuthMVC.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(UserName), IsUnique = true)]
+    public class UserAccount
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(30, ErrorMessage ="Max of 30 chracters is allowed")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(30, ErrorMessage = "Max of 30 chracters is allowed")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [MaxLength(60, ErrorMessage = "Max of 60 chracters is allowed")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20, ErrorMessage = "Max of 20 chracters is allowed")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+}
