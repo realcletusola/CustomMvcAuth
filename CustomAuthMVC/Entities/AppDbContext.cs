f8f28b1437dd1ae97c9ca25c419feac776d038da
cletus <cletushunsu@gmail.com>
2024-08-27 08:28:32 -0700

    created DbContext

---
+++ CustomAuthMVC/Entities/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomAuthMVC.Entities
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
