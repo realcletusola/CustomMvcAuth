8bb8e9dac6c3d28228e143f8b29c06db4109218d
cletus <cletushunsu@gmail.com>
2024-08-28 05:53:50 -0700

    created UserRegistration model

---
+++ CustomAuthMVC/Models/RegistrationModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomAuthMVC.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(30, ErrorMessage = "Max of 30 chracters is allowed")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(30, ErrorMessage = "Max of 30 chracters is allowed")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(60, MinimumLength =5, ErrorMessage = "Max of 60 and min of 5 chracters is allowed")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Please provide a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20, ErrorMessage = "Max of 20 chracters is allowed")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "Password must be at least 8 charcters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Both passwords must be equal")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "Password must be at least 8 charcters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
